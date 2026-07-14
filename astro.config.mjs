6b60e0d0ccc008c5b8b36404cf796d9274cc9a38
Shriya <shriyajegan@gmail.com>
2026-07-14 17:26:01 -0500

    added config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://shriyaj7809.github.io",
+  base: "/devportfolio/",
   vite: {
     plugins: [tailwindcss()],
   },

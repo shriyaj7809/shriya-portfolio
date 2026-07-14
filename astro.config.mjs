25699340d2a8d37b69b566d145dedde4b054facc
Shriya <shriyajegan@gmail.com>
2026-07-14 17:28:52 -0500

    Revert "added config"
    This reverts commit 6b60e0d0ccc008c5b8b36404cf796d9274cc9a38.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://shriyaj7809.github.io",
-  base: "/devportfolio/",
   vite: {
     plugins: [tailwindcss()],
   },
